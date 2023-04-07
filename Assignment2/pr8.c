5abdfda63ffb7b6a7c9b27d1dfa7451e8f3cc8f2
Ansu-mann <124277891+Ansu-mann@users.noreply.github.com>
2023-04-07 22:12:09 +0530

    Create pr8.c

---
+++ Assignment2/pr8.c
@@ -0,0 +1,44 @@
+//array input and display using pointers
+#include<stdlib.h>
+#include<stdio.h>
+int* create_Array(int *a,int n){
+    int i;
+    a=(int*)malloc(n*sizeof(int));
+    printf("Enter the elements of the array:\n");
+    for(i=0;i<n;i++){
+        scanf("%d",(a+i));
+    }
+    return a;
+}
+void display_Array(int *a,int n){
+    int i;
+    printf("The array:\n");
+    for(i=0;i<n;i++){
+        printf("%d\t",*(a+i));
+    }
+}
+int reverse(int *a,int n){
+    int i,j,c;
+    for(i=0,j=n-1;i<n/2;i++,j--){
+        c=*(a+i);
+        *(a+i)=*(a+j);
+        *(a+j)=c;
+    }
+    return a;
+}
+void display_Reversed_Array(int *a,int n){
+    int i;
+    for(i=0;i<n;i++){
+        printf("%d\t",*(a+i));
+    }
+}
+int main(){
+    int *a,n;
+    printf("Enter the length of the array:");
+    scanf("%d",&n);
+    a=create_Array(a,n);
+    display_Array(a,n);
+    a=reverse(a,n);
+    display_Reversed_Array(a,n);
+    return 0;
+}
