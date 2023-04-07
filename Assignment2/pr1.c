1705bc2c9fd098f40bf36ea3802276faddb8bde1
Ansu-mann <124277891+Ansu-mann@users.noreply.github.com>
2023-04-07 21:06:16 +0530

    Update pr1.c

--- Assignment2/pr1.c
+++ Assignment2/pr1.c
@@ -1,1 +1,9 @@
-
+//value and address of variable without pointer
+#include <stdio.h>
+int main(){
+    int x;
+    printf("Enter a number:");
+    scanf("%d",&x);
+    printf("value=%d,address=%u",x,&x);
+    return 0;
+}
