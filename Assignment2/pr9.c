adf2d20f06592df48f7bc6d5c85e9279df4ef7b7
Ansu-mann <124277891+Ansu-mann@users.noreply.github.com>
2023-04-13 20:39:12 +0530

    Update pr9.c

--- Assignment2/pr9.c
+++ Assignment2/pr9.c
@@ -2,86 +2,84 @@
 #include<stdio.h>
 #include<stdlib.h>
 int *create_Array(int *a,int n){
-    int i;
-    a=(int*)malloc(n*sizeof(int));
-    printf("Enter the elements of the array:\n");
-    for(i=0;i<n;i++){
-        scanf("%d",(a+i));
-    }
+	int i;
+	a=(int*)malloc(n*sizeof(int));
+	printf("\nEnter the elements of the array:\n");
+	for(i=0;i<n;i++){
+		scanf("%d",(a+i));
+	}
+	return a;
 }
-void display_Array(int *a,int n){
-    int i;
-    printf("The array:\n");
-    for(i=0;i<n;i++){
-        printf("%d\t",*(a+i));
-    }
+void display_Array(int *b,int n){
+	int i;
+	printf("\nThe elements of the array:\n");
+	for(i=0;i<n;i++){
+		printf("%d\t",*(b+i));
+	}
 }
 void sort_Array(int *a,int n){
-    int i,j,c;
-    for(i=0;i<n;i++){
-        for(j=0;j<n-1;j++){
-            if(*(a+i)>*(a+j)){
-                c=*(a+i);
-                *(a+i)=*(a+j);
-                *(a+j)=c;
-            }
-        }
-    }
-    printf("The sorted array:\n");
-        for(i=0;i<n;i++){
-            printf("%d\t",*(a+i));
-        }
+	int i,j,c;
+	for(i=0;i<n;i++){
+		for(j=0;j<n;j++){
+			if(*(a+i)<*(a+j)){
+				c=*(a+i);
+				*(a+i)=*(a+j);
+				*(a+j)=c;
+			}
+		}
+	}
+	printf("\nThe sorted array:\n");
+	for(i=0;i<n;i++){
+		printf("%d\t",*(a+i));
+	}
 }
 void linear_Search(int *a,int n){
-    int i,key,flag=0;
-    printf("Enter the element to be searched:");
-    scanf("%d",&key);
-    for(i=0;i<n;i++){
-        if(*(a+i)==key){
-            printf("The element %d is found at %d index",key,i);
-            break;
-        }
-        else{
-            flag=1;
-            break;
-        }
-    }
-    if(flag==1){
-        printf("The element %d is not found",key);
-    }
+	int i,k,flag=0;
+	printf("\nEnter the element you want to search:");
+	scanf("%d",&k);
+	for(i=0;i<n;i++){
+		if(*(a+i)==k){
+			printf("\nThe element %d is found at %d index",k,i);
+			flag=1;
+			break;
+		}
+	}
+	if(flag==0){
+		printf("\nThe element not found");
+	}
 }
 int main(){
-    int *a,n,choice;
-    do{
-        printf("for creating an array: 1\n");
-        printf("for displaying  the array: 2\n");
-        printf("for sorting the array in ascending order: 3\n");
-        printf("for searching an element in the array: 4\n");
-        printf("for exiting the menu: 5\n");
-        printf("Enter your choice:");
-        scanf("%d",&choice);
-        switch(choice){
-            case 1:
-            printf("Enter the lenght of the array:");
-            scanf("%d",&n);
-            a=create_Array(a,n);
-            break;
-            case 2:
-            display_Array(a,n);
-            break;
-            case 3:
-            sort_Array(a,n);
-            break;
-            case 4:
-            linear_Search(a,n);
-            break;
-            case 5:
-            printf("program exited");
-            break;
-            defalut:
-            printf("Invalid choice");
-        }
-    }
-    while(choice!=5);
-    return 0;
+	int *a,n,choice;
+	do{	
+		printf("\nMENU:");
+		printf("\n1: create an array");
+		printf("\n2: display the array");
+		printf("\n3: sort the array");
+		printf("\n4: Linear search");
+		printf("\n5: Exit the program");
+		printf("\nEnter your choice:");
+		scanf("%d",&choice);
+		switch(choice){
+			case 1:
+				printf("Enter the length of the array:");
+				scanf("%d",&n);
+				a=create_Array(a,n);
+				break;
+			case 2:
+				display_Array(a,n);
+				break;
+			case 3:
+				sort_Array(a,n);
+				break;
+			case 4:
+				linear_Search(a,n);
+				break;
+			case 5:
+				printf("\nProgram terminated\n\n");
+				break;
+			default:
+				printf("Invalid option\n");
+		}
+	}while(choice!=5);
+	return 0;
 }
