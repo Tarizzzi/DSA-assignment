b530fc5251a9bad66cd3b02d1de5e5acb08babd5
Ansu-mann <124277891+Ansu-mann@users.noreply.github.com>
2023-04-13 21:47:11 +0530

    Create pr9_alternate.c

---
+++ Assignment2/pr9_alternate.c
@@ -0,0 +1,89 @@
+//menu driven program in function(timepass method)
+#include<stdio.h>
+#include<stdlib.h>
+int *create_Array(int *a,int n){
+	int i;
+	a=(int*)malloc(n*sizeof(int));
+	printf("\nEnter the elements of the array:\n");
+	for(i=0;i<n;i++){
+		scanf("%d",(a+i));
+	}
+	return a;
+}
+void display_Array(int *a,int n){
+	int i;
+	printf("\nThe elements of the array:\n");
+	for(i=0;i<n;i++){
+		printf("%d\t",*(a+i));
+	}
+}
+int *sort_Array(int *a,int n){
+	int i,j,c;
+	for(i=0;i<n;i++){
+		for(j=0;j<n;j++){
+			if(*(a+i)<*(a+j)){
+				c=*(a+i);
+				*(a+i)=*(a+j);
+				*(a+j)=c;
+			}
+		}
+	}
+	return a;
+}
+int linear_Search(int *a,int n,int k){
+	int i,flag=0;
+	for(i=0;i<n;i++){
+		if(*(a+i)==k){
+		    flag=1;
+		    return i;
+		}
+	}
+	if(flag==0){
+	printf("\nThe element not found");   
+	}
+}
+void switchcase(int *a,int n,int choice,int i,int k){
+    do{	
+		printf("\nMENU:");
+		printf("\n1: create an array");
+		printf("\n2: display the array");
+		printf("\n3: sort the array");
+		printf("\n4: Linear search");
+		printf("\n5: Exit the program");
+		printf("\nEnter your choice:");
+		scanf("%d",&choice);
+		switch(choice){
+			case 1:
+				printf("Enter the length of the array:");
+				scanf("%d",&n);
+				a=create_Array(a,n);
+				break;
+			case 2:
+				display_Array(a,n);
+				break;
+			case 3:
+				a=sort_Array(a,n);
+				printf("\nThe sorted array:\n");
+	            for(i=0;i<n;i++){
+		            printf("%d\t",*(a+i));
+	            }
+				break;
+			case 4:
+			    printf("\nEnter the element you want to search:");
+	            scanf("%d",&k);
+				i=linear_Search(a,n,k);
+			    printf("\nThe element %d is found at %d index",k,i);
+				break;
+			case 5:
+				printf("\nProgram terminated\n\n");
+				break;
+			default:
+				printf("Invalid option\n");
+		}
+	}while(choice!=5);
+}
+int main(){
+	int *a,n,choice,i,k;
+	switchcase(a,n,choice,i,k);
+	return 0;
+}
