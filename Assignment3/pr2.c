772e368cced6c1ef634abd0af1dce9b3d195ea68
Ansu-mann <124277891+Ansu-mann@users.noreply.github.com>
2023-04-15 22:56:17 +0530

    Update pr2.c

--- Assignment3/pr2.c
+++ Assignment3/pr2.c
@@ -1,33 +1,33 @@
 //triplet to sparse
 #include<stdio.h>
 #define max 20
-void read_Triplet(int a[][3],int m){
+void read_Triplet(int a[][3]){
     int i,j;
     printf("Enter the elements of the triplet:\n");
-    for(i=0;i<m;i++){
+    for(i=0;i<a[0][2]+1;i++){
         for(j=0;j<3;j++){
             scanf("%d",&a[i][j]);
         }
     }
 }
-void display_Triplet(int a[][3],int m){
+void display_Triplet(int a[][3]){
     int i,j;
     printf("The input triplet matrix:\n");
-    for(i=0;i<m;i++){
+    for(i=0;i<a[0][2]+1;i++){
         for(j=0;j<3;j++){
             printf("%d\t",a[i][j]);
         }
         printf("\n");
     }
 }
-void convert_SM(int a[][3],int m,int b[][max]){
+void convert_SM(int a[][3],int b[][max]){
     int i,j;
     for(i=0;i<a[0][0];i++){
         for(j=0;j<a[0][1];j++){
             b[i][j]=0;
         }
     }
-    for(i=1;i<m;i++){
+    for(i=1;i<a[0][2]+1;i++){
         b[a[i][0]][a[i][1]]=a[i][2];
         }
 }
@@ -42,12 +42,10 @@ void display_SM(int a[][3],int b[][max]){
     }
 }
 int main(){
-    int a[max][3],b[max][max],m;
-    printf("Enter the rows of the triplet matrix:");
-    scanf("%d",&m);
-    read_Triplet(a,m);
-    display_Triplet(a,m);
-    convert_SM(a,m,b);
+    int a[max][3],b[max][max];
+    read_Triplet(a);
+    display_Triplet(a);
+    convert_SM(a,b);
     display_SM(a,b);
     return 0;
 }
