b4120d37958d147000a1a6853b8fb749c7ddd340
ANSUMAN PANDA <124277891+Ansu-mann@users.noreply.github.com>
2023-07-14 07:55:07 +0530

    Update pr6.c

--- Assignment9/pr6.c
+++ Assignment9/pr6.c
@@ -1,3 +1,4 @@
+//merge sort
 #include<stdio.h>
 #include<stdlib.h>
 void input(int a[], int n)
@@ -9,45 +10,34 @@ void input(int a[], int n)
 		scanf("%d",&a[i]);
 	}
 }
-void merge(int a[], int lb,int mid, int ub)
-{
-	int i=lb,j=mid+1,k=lb, c[50];
-	while(i <= mid && j <= ub)
-	{
-		if(a[i] < a[j])
-		{
-			c[k++]=a[i];
-			i++;
-		}
-		else if(a[i] > a[j])
-		{
-			c[k++] = a[j];
-			j++;
-		}
-	}
-	while(i<=lb)
-	{
-		c[k++] = a[i++];
-	}
-	while(j <= ub)
-	{
-		c[k++] = a[j++];
-	}
-	for(i = lb; i <= ub; i++)
-		a[i] = c[i];
+void merge(int a[], int lb, int mid, int ub) {
+    int i = lb, j = mid + 1, k = lb, c[50];
+    while (i <= mid && j <= ub) {
+        if (a[i] < a[j]) {
+            c[k++] = a[i++];
+        } else {
+            c[k++] = a[j++];
+        }
+    }
+    while (i <= mid) {
+        c[k++] = a[i++];
+    }
+    while (j <= ub) {
+        c[k++] = a[j++];
+    }
+    for (i = lb; i <= ub; i++) {
+        a[i] = c[i];
+    }
 }
-void mergeSort(int a[], int lb,int ub)
-{
-	int mid;
-	if(lb < ub)
-	{
-		mid = (lb + ub) / 2;
-		mergeSort(a, lb, mid);
-		mergeSort(a, mid + 1, ub);
-		merge(a, lb, mid, ub);
-	}
+void mergeSort(int a[], int lb, int ub) {
+    int mid;
+    if (lb < ub) {
+        mid = (lb + ub) / 2;
+        mergeSort(a, lb, mid);
+        mergeSort(a, mid + 1, ub);
+        merge(a, lb, mid, ub);
+    }
 }
-
 void display(int a[], int n)
 {
 	int i;
