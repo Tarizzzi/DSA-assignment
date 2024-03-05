e607d962def68aa9aa6dddcebdda05ec4d73a687
ANSUMAN PANDA <124277891+Ansu-mann@users.noreply.github.com>
2024-03-05 08:34:10 +0530

    Update pr13.c

--- Assignment1/pr13.c
+++ Assignment1/pr13.c
@@ -16,25 +16,27 @@ void display_Array(int a[],int n){
 }
 void binary_Search(int a[],int n){
     int i,k,flag=0;
-    printf("\nEnter the value to be searched:");
+    printf("Enter the value to be searched:");
     scanf("%d",&k);
-    if(k==a[n/2])
-    printf("%d is found at %d position",k,(n+2)/2);
-    else if(k>n/2){
+    if(k==a[n/2]){
+        printf("%d is found at %d position",k,(n+2)/2);
+        flag = 1; 
+    }
+    else if(k>a[n/2]){
         for(i=(n+2)/2;i<n;i++){
             if(a[i]==k){
                 printf("%d is found at %d position",k,i+1);{
-                    flag=1;
+                    flag = 2;
                     break;
                 }
             }
         }
     }
-    else if(k<n/2){
+    else if(k<a[n/2]){
         for(i=0;i<n/2;i++){
             if(a[i]==k){
                 printf("%d is found at %d position",k,i+1);{
-                    flag=2;
+                    flag = 3;
                     break;
                 }
             }
@@ -49,7 +51,7 @@ int main(){
     printf("Enter the length of the array:");
     scanf("%d",&n);
     create_Array(a,n);
-    display_Array(a,n);
+    // display_Array(a,n);
     binary_Search(a,n);
     return 0;
 }
